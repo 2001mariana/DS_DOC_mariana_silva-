80bd9385f8ac5ee4b87917111e13c68997460e30
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-10-05 14:32:16 -0300

    estilizando pagina notFound

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,10 +1,27 @@
+import Lottie from 'lottie-react'
+
+import NotFoundAnimation from '../../assets/animations/penguin-hello.json'
+import ArrowAnimation from '../../assets/animations/pink-arrow.json'
+
 import './NotFound.css'
 
 const NotFound = () => {
-    return (<div className='NotFound'>
-        <h2 className="NotFound__message-title">Página não encontrada.</h2>
-        <p className="NotFound__message-body">Verifique se sua busca está correta e tente novamente.</p>
-    </div>)
+    return (<>
+    <div className="NotFound__animation--arrow">
+        <Lottie animationData={ArrowAnimation} loop={true} />
+    </div>
+    <div className='NotFound'>
+        <div className="NotFound__message">
+            <h2 className="NotFound__message-title">Que bom ter você aqui!</h2>
+            <p className="NotFound__message-body">Escolha um dos itens para continuar.</p>
+        </div>
+        
+        <div className="NotFound__animation--penguin">
+            <Lottie animationData={NotFoundAnimation} loop={true} />
+        </div>
+
+    </div></>
+    )
 }
 
 export default NotFound
