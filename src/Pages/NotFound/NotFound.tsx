6d42fd62a9396c22566b51d597e3152cd9b6601f
mariana.luiza <mariana.luiza@spcbrasil.org.br>
2023-02-01 18:08:15 -0300

    removendo arquivos desnecessarios

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,21 +1,16 @@
 import Lottie from 'lottie-react'
 
-import ArrowAnimation from '../../assets/animations/pink-arrow.json'
 import WelcomeAnimation from '../../assets/animations/welcome.json'
 
 import './NotFound.css'
 
 const NotFound = () => {
-    return (<>
-    <div className="NotFound__animation--arrow">
-        <Lottie animationData={ArrowAnimation} loop={true} />
-    </div>
+    return (
     <div className='NotFound__container'>
       <div className="NotFound__animation--welcome">
         <Lottie animationData={WelcomeAnimation} loop={true} />
       </div>
     </div>
-    </>
     )
 }
 
